9e99c2b2737404b5ed5d7e39f45d567bc9088917
Varun <1hvarun@gmail.com>
2018-03-06 20:52:04 +0530

    added code formatting

--- concordance.js
+++ concordance.js
@@ -1,18 +1,25 @@
-const sentenceMapper = (sentences) => {
-  return sentences.reduce((map,sentence,index) => {
-      return sentence.split(" ").reduce((map,word) => {
-        if(word.trim().length < 1) return map;
-        const sentenceList = map[word] || [];
-        map[word] = sentenceList.concat(index);
-        return map;
-      },map);
-  },{}); 
-}
+const sentenceTokenizer = (abbreviations, document) => {
+  return document
+    .split(" ")
+    .map(word => {
+      return abbreviations.indexOf(word) !== -1 ? word.replace(".", "@") : word;
+    })
+    .join(" ")
+    .split(".")
+    .map(sentence => {
+      return sentence.replace("@", ".");
+    });
+};
 
-const sentenceTokenizer = (abbreviations,document) => {
-  return document.split(" ").map((word) => {
-	  return	abbreviations.indexOf(word) !== -1 ? word.replace(".","@") : word;
-  }).join(" ").split(".").map((sentence) => {
- 		return sentence.replace("@",".");
- });
-}
+const sentenceMapper = sentences => {
+  return sentences.reduce((map, sentence, index) => {
+    return sentence.split(" ").reduce((map, word) => {
+      const sentenceList = map[word] || [];
+      map[word] =
+        sentenceList.indexOf(index) === -1
+          ? sentenceList.concat(index)
+          : sentenceList;
+      return map;
+    }, map);
+  }, {});
+};
