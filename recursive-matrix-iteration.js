dedf7b090a95a1285c7e0b470b4acb91449f173c
Varun <1hvarun@gmail.com>
2018-03-07 10:23:55 +0530

    Create recursive-matrix-iteration.js

---
+++ recursive-matrix-iteration.js
@@ -0,0 +1,32 @@
+const currentX = 1;
+const currentY = 1;
+const ROWS = 3;
+const COLUMNS = 3;
+
+const isReachable = (currentX, currentY, i, j) => {
+  let isReachable = false;
+  try {
+    const slope = (j - currentY) / (i - currentX);
+    isReachable = Math.abs(slope) === 1;
+  } catch (e) {}
+  return isReachable;
+};
+
+const iterateMatrix = (matrix, i, j, paths = []) => {
+  if (i > ROWS-1) {
+    return;
+  }
+  if (j > COLUMNS - 1) {
+    i = i + 1;
+    j = 0;
+  }
+  if (i < ROWS && j < COLUMNS) {
+    if (isReachable(currentX, currentY, i, j)) {
+      paths.push([i, j]);
+    }
+  }
+
+  iterateMatrix(matrix, i, j + 1, paths);
+};
+
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
