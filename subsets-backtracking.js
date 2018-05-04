7edace29e28a900fa98b6b71f80d36f5ccc61322
Varun <1hvarun@gmail.com>
2018-05-04 18:00:32 +0530

    Update subsets-backtracking.js

--- subsets-backtracking.js
+++ subsets-backtracking.js
@@ -1,15 +1,14 @@
-const generateSubsets = (superset, start) => {
+const generateSubsets = (superset) => {
   subset.length && subsets.push(subset);
-  for (let i = start; i < superset.length; i++) {
+  for (let i = 0; i < superset.length; i++) {
     subset = subset.concat(superset[i]);
-    generateSubsets(superset, i + 1);
+    generateSubsets(superset.slice(i + 1));
     subset = subset.slice(0, subset.length - 1)
   }
 }
 
-
 let subsets = [];
 let subset = [];
-const superset = [1, 2, 3];
-generateSubsets(superset, 0);
-console.log(subsets)
+const superset = "abc";
+generateSubsets(superset);
+console.log(subsets);
