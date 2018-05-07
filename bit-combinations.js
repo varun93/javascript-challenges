889bdb81c7c8abbe3a0997d9e42d91edaffb3871
Varun <1hvarun@gmail.com>
2018-05-07 15:08:30 +0530

    Update bit-combinations.js

--- bit-combinations.js
+++ bit-combinations.js
@@ -1,15 +1,11 @@
 const printBinary = (n, result) => {
-   if (n === 0) {
+    if (n === 0) {
         console.log(result)
     }
     else {
-        result = result.concat(0);
-        printBinary(n - 1, result);
-        // unchoose 0; and choose 1
-        result = result.slice(0, result.length - 1);
-        result = result.concat(1);
-        printBinary(n - 1, result);
+        printBinary(n - 1, result.concat(0));
+        printBinary(n - 1, result.concat(1));
     }
 }
 
-printBinary(3, []);
+printBinary(2, []);
