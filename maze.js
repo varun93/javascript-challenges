eed3aa67c51cfd29db8cc4e4a20ee7762372a636
Varun <1hvarun@gmail.com>
2018-05-09 10:36:02 +0530

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,5 +1,3 @@
-const ROWS = 4;
-const COLS = 4;
 const VISITED = 1;
 const UNVISITED = 0;
 const BLOCKED = -1;
@@ -9,32 +7,53 @@ const isSafe = (startX, startY) => {
   return (
     startX >= 0 &&
     startY >= 0 &&
-    startX < ROWS &&
-    startY < COLS &&
+    startX < maze.length &&
+    startY < maze[startX].length &&
     maze[startX][startY] !== VISITED &&
     maze[startX][startY] !== BLOCKED
   );
 };
 
+const visitCell = (startX, startY) => {
+  maze[startX][startY] = VISITED;
+  path.push({ startX, startY });
+};
+
 const travelMaze = (startX, startY) => {
+  console.log(startX, startY);
+
   if (isSafe(startX, startY)) {
     //if it reaches the destination
     if (maze[startX][startY] === DESTINATION) {
-      console.log(path.concat({ startX, startY }));
+      path.push({ startX, startY });
+      console.log(path);
       return true;
     } else {
-      maze[startX][startY] = VISITED;
-      path.push({ startX, startY });
-      //go forward
-      if (travelMaze(startX + 1, startY)) {
-        return true;
-      }
-      //go down
+      // visit the cell
+      visitCell(startX, startY);
+
+      //go front
       if (travelMaze(startX, startY + 1)) {
         return true;
       }
+
+      //go back
+      if (travelMaze(startX, startY - 1)) {
+        return true;
+      }
+
+      //go up
+      if (travelMaze(startX - 1, startY)) {
+        return true;
+      }
+
+      //go down
+      if (travelMaze(startX + 1, startY)) {
+        return true;
+      }
+
       // remove the path
-      path = path.slice(0, path.length - 1);
+      path.pop();
       return false;
     }
   }
@@ -43,12 +62,22 @@ const travelMaze = (startX, startY) => {
 
 let path = [];
 
+//solution possible if the rat can move just in a couple of directions
 const maze = [
   [UNVISITED, BLOCKED, BLOCKED, BLOCKED],
   [UNVISITED, UNVISITED, UNVISITED, UNVISITED],
   [BLOCKED, UNVISITED, BLOCKED, BLOCKED],
   [UNVISITED, UNVISITED, UNVISITED, DESTINATION]
 ];
+
+// solution possible only if it moves in all directions
+// const maze = [
+//   [UNVISITED, BLOCKED, UNVISITED, UNVISITED, UNVISITED],
+//   [UNVISITED, UNVISITED, UNVISITED, BLOCKED, UNVISITED],
+//   [UNVISITED, BLOCKED, BLOCKED, UNVISITED, UNVISITED],
+//   [UNVISITED, BLOCKED, BLOCKED, UNVISITED, BLOCKED],
+//   [UNVISITED, BLOCKED, BLOCKED, UNVISITED, DESTINATION]
+// ];
 
 // const maze = [
 //   [BLOCKED, UNVISITED, UNVISITED, UNVISITED, BLOCKED, UNVISITED],
@@ -61,3 +90,7 @@ const maze = [
 // console.log(maze);
 travelMaze(0, 0);
 // console.log(maze);
+// path.concat and path.slice clones creates a new copy of the array
+// path.pop and path.push mutates the array so modifies the array; there is always one copy of the array available
+// string works because string by default is immutable by nature
+// return always returns to the caller
