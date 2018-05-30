34a11c29be0d636ddb647496b888956fbe123040
Varun <1hvarun@gmail.com>
2018-05-30 10:10:52 +0530

    Update generalized-curry.js

--- generalized-curry.js
+++ generalized-curry.js
@@ -1,23 +1,21 @@
-const curry = (fn, args = []) => {
+const curry = (fn) => {
     if (fn.length === 0) {
         return fn;
     }
-
-    return (...ags) => {
-        args.push(...ags);
-        if (args.length === fn.length) {
-            return fn(...args);
-        }
-        else {
-            return curry(fn, args);
+    const args = [];
+    const nest = () => {
+        return (...ags) => {
+            args.push(...ags);
+            return args.length === fn.length ? fn(...args) : nest();
         }
     };
+    return nest();
 }
 
 // final function
 const sum = curry((x, y, z, a, b) => {
     return x + y + z + a + b;
-}, []);
+});
 
 const total = sum(1)(2)(3)(4, 5);
 console.log(total);
