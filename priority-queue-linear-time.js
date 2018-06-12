15837e4f5419b11356848d3a8c7589b2c1ac24e2
Varun <1hvarun@gmail.com>
2018-06-12 11:02:58 +0530

    removed console.log and formatted the code

--- priority-queue-linear-time.js
+++ priority-queue-linear-time.js
@@ -1,63 +1,61 @@
-const insert = (array,index,element) => {
-		
-    let runner = array.length-1;
+const insert = (array, index, element) => {
 
-  	while(runner >= index) {
-    	array[runner + 1] = array[runner];
-      runner--;
+    let runner = array.length - 1;
+
+	while (runner >= index) {
+        array[runner + 1] = array[runner];
+        runner--;
     }
-    
+
     array[index] = element;
 }
 
 
-const priorityQueue = (array,element, start, end) => {
-	
-  start = start || 0;
-  end = end || array.length -1;
-  
-  console.log(start,end);
-  if(start > end){
-	 insert(array,end+1,element);
-  	return;
-  }
-  
-  const mid = Math.floor(start + (end - start)/2);
-  const midElement = array[mid];
-  
-  if(element >= midElement){
-  	if(element < array[mid-1] ){
-      insert(array,mid,element);
-    	return;
+const priorityQueue = (array, element, start, end) => {
+
+    start = start || 0;
+    end = end || array.length - 1;
+
+    if (start > end) {
+        insert(array, end + 1, element);
+        return;
     }
-    return priorityQueue(array,element,start,mid-1);
-  }
-	
-   if(element < midElement){
-  	if(element >= array[mid+1]){
-         insert(array,mid+1,element);
-         return;
+
+    const mid = Math.floor(start + (end - start) / 2);
+    const midElement = array[mid];
+
+    if (element >= midElement) {
+        if (element < array[mid - 1]) {
+            insert(array, mid, element);
+            return;
+        }
+        return priorityQueue(array, element, start, mid - 1);
     }
-    return priorityQueue(array,element,mid+1,end);
-  }
 
-} 
+    if (element < midElement) {
+        if (element >= array[mid + 1]) {
+            insert(array, mid + 1, element);
+            return;
+        }
+        return priorityQueue(array, element, mid + 1, end);
+    }
 
-// the search is linear
-const priorityQueueWithLinearSearch = (array,element) => {
-     let i = array.length -1;
-     while(element >= array[i]){
-      i--;
-     }
-     insert(array,i+1,element)
- }
+}
 
- const array = [];
- 
- priorityQueue(array,4); 
- priorityQueue(array,8);
- priorityQueue(array,1);
- priorityQueue(array,7);
- priorityQueue(array,3);
+const priorityQueueWithLinearSearch = (array, element) => {
+    let i = array.length - 1;
+    while (element >= array[i]) {
+        i--;
+    }
+    insert(array, i + 1, element)
+}
+
+const array = [];
+
+priorityQueue(array, 4);
+priorityQueue(array, 8);
+priorityQueue(array, 1);
+priorityQueue(array, 7);
+priorityQueue(array, 3);
 
 console.log(array);
