6382069ef37bd62607f727fd6912cc25e15ac8b8
Varun <1hvarun@gmail.com>
2018-06-19 12:07:01 +0530

    Update print-binary.js

--- print-binary.js
+++ print-binary.js
@@ -1,3 +1,14 @@
 	const toBinaryRecursive = (num) => {
 		return num < 2 ? num + "" : toBinaryRecursive(num >> 1) + num % 2;
 	}
+
+	const toBinary = (num) => {
+
+		let bits = [], mask = 1;
+
+		while (mask <= num) {
+			bits.unshift(num & mask ? 1 : 0);
+			mask <<= 1;
+		}
+		return bits.join("");
+	}
