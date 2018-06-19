654d37d63a4033e2c8bd8d289f33f87f2af62d36
Varun <1hvarun@gmail.com>
2018-06-19 12:05:50 +0530

    Update print-binary.js

--- print-binary.js
+++ print-binary.js
@@ -1,11 +1,3 @@
-const printBinary = number => {
-  number = Math.floor(number);
-  if (number < 2) {
-    return number + "";
-  } else {
-    return printBinary(number / 2) + number % 2;
-  }
-};
-
-const number = 43;
-console.log(printBinary(number));
+	const toBinaryRecursive = (num) => {
+		return num < 2 ? num + "" : toBinaryRecursive(num >> 1) + num % 2;
+	}
