9f884aa233cac32423828c380880842f601890bf
Varun <1hvarun@gmail.com>
2018-06-26 13:14:45 +0530

    Update gcd.js

--- gcd.js
+++ gcd.js
@@ -1,26 +1,21 @@
-const euclideanGCD = (a,b) => {
-		
-	if(b > a){
-  	a = a ^ b;
+const euclideanGCD = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  // swap if a is lesser than b
+  if (b > a) {
+    a = a ^ b;
     b = a ^ b;
     a = a ^ b;
   }
-  
-  if(b === 0){
-	  return a;
+  return euclideanGCD(b, a % b);
+};
+
+const gcd = (a, b) => {
+  if (a === 0) return b;
+  if (b === 0) return a;
+
+  for (let i = Math.min(a, b) >> 1; i >= 1; i--) {
+    if (a % i === 0 && b % i === 0) return i;
   }
-
-  return euclideanGCD(b,a%b);
-  
-}
-
-const gcd = (a,b) => {
-  
-  if(a === 0) return b;
-  if(b === 0) return a;
-  
-	for(let i = Math.min(a,b)>>1; i >= 1; i--){
-  	if(a%i === 0  && b%i === 0)  return i;
-  }
-
-}
+};
